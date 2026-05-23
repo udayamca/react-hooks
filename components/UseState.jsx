5a002c84bcd3cf0f834132a912a389faed368373
udaya <udayamca@gmail.com>
2026-05-23 15:23:07 -0700

    adding useState with react

---
+++ components/UseState.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function UseState() {
+  const [count, setCount] = useState(100);
+  const increase = (value) => {
+    setCount((prev) => prev + value);
+  };
+  const decrease = (value) => {
+    setCount((prev) => prev - value);
+  };
+  return (
+    <>
+      <div className="usestate" style={{ textAlign: "left" }}>
+        <h2>useState</h2>
+        <div className="usestate-cont">
+          useState — stores a value inside a component. Calling the setter
+          re-renders the component with the new value. For objects, always
+          spread previous state:{" "}
+          <span>setUser(prev =&gt; ( ...prev, age : 26 ))</span>. useState lets
+          a component remember values between renders. It returns a pair: the
+          current value and a setter function. Calling the setter triggers a
+          re-render with the new value.
+        </div>
+        <br />
+        <div style={{ fontWeight: "bold" }}>Count: {count}</div>
+        <button onClick={() => increase(100)}>Increase</button>
+        <button onClick={() => decrease(50)}>Decrease</button>
+      </div>
+    </>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import UseState from "../components/UseState";
+
+function App() {
+  return (
+    <>
+      <h1>React Hooks</h1>
+      <UseState></UseState>
+    </>
+  );
+}
+
+export default App;
