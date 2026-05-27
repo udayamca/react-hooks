966e35c6b832250dadcf799a0b287c287c0e0be8
udaya <udayamca@gmail.com>
2026-05-26 20:01:45 -0700

    display the enhance data

--- components/sub-components/HigherOrderEnhancement.jsx
+++ components/sub-components/HigherOrderEnhancement.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 const HigherOrderEnhancement = (Comp) => {
+  const [info, setInfo] = useState("");
   return (props) => {
     console.log("props:", props);
     const handleClick = () => {
       console.log("Handle Click: ", props.info);
+      setInfo(props.info);
     };
 
     return (
-      <span onClick={handleClick}>
-        <Comp {...props} />
-      </span>
+      <>
+        <span onClick={handleClick}>
+          <Comp {...props} />
+        </span>
+        <h3>{info.name}</h3>
+        <h3>{info.age}</h3>
+        <h3>{info.city}</h3>
+      </>
     );
   };
 };
